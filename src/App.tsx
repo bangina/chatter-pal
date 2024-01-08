e36fd25548e65eca8a104e3c7ec2349c62916c04
bangina <bbangina@gmail.com>
2024-01-08 11:17:41 +0900

    Setup Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { RecoilRoot } from "recoil";
 import Prestudy from "./pages/Prestudy";
 
 function App() {
   return (
-    <main className="min-h-screen bg-gray-100 p-[20px]">
-      <Prestudy />
-    </main>
+    <RecoilRoot>
+      <main className="min-h-screen bg-gray-100 p-[20px]">
+        <Prestudy />
+      </main>
+    </RecoilRoot>
   );
 }
 
