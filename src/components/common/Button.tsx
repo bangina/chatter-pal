36616c2b551099eba09c1986d71a6b0bdb651ff6
bangina <bbangina@gmail.com>
2024-01-08 11:51:08 +0900

    Setup OpenAI key

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,8 +1,9 @@
-function Button({ label }: { label: string }) {
+function Button({ label, onClick }: { label: string; onClick?: () => void }) {
   return (
     <button
       type="button"
       className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-[6px] px-[12px] rounded"
+      onClick={onClick}
     >
       {label}
     </button>
--- src/pages/Prestudy.tsx
+++ src/pages/Prestudy.tsx
@@ -2,11 +2,15 @@ import Button from "../components/common/Button";
 import GeneratedScript from "../components/prestudy/GeneratedScript";
 import MyScript from "../components/prestudy/MyScript";
 
+const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 function Prestudy() {
+  const handleClickGrammarCheck = () => {
+    
+  };
   return (
     <>
       <MyScript />
-      <Button label="문법 체크✅" />
+      <Button onClick={handleClickGrammarCheck} label="문법 체크✅" />
       <Button label="어휘 향상🌈" />
       <Button label="대화 버전💬" />
       <GeneratedScript />
