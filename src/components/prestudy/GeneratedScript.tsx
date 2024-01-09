63c8703e878cd8895cc09193aa0af497f86cae55
bangina <bbangina@gmail.com>
2024-01-09 16:17:31 +0900

    Add audio play/pause functionality

--- src/components/prestudy/GeneratedScript.tsx
+++ src/components/prestudy/GeneratedScript.tsx
@@ -2,8 +2,13 @@ import { useRecoilValue } from "recoil";
 import Button from "../common/Button";
 import Title from "../common/Title";
 import { generatedScriptState } from "../../recoil";
+import openAIClient from "../../api/openai/openAIClient";
+import { useState } from "react";
+
 function GeneratedScript() {
   const generatedScript = useRecoilValue(generatedScriptState);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
   const parsedScript = (script: string) => {
     if (!script.includes("[original_starts]")) return script;
     return script
@@ -34,7 +39,50 @@ function GeneratedScript() {
       alert("복사에 실패했습니다.");
     }
   };
+  const playAudio = (audio: HTMLAudioElement) => {
+    audio.play();
+    setIsPlaying(true);
+  };
+  const pauseAudio = (audio: HTMLAudioElement) => {
+    audio.pause();
+    setIsPlaying(false);
+  };
+  const createAndPlayAudio = (audioURL: string) => {
+    const newAudio = new Audio(audioURL);
+    setAudio(newAudio);
+    playAudio(newAudio);
+  };
+  const handleClickTTS = async (script: string) => {
+    // if the audio is already in the sessionstorage, play it
+    const storedAudioURL = sessionStorage.getItem("audio");
+    if (storedAudioURL) {
+      if (!audio) {
+        createAndPlayAudio(storedAudioURL);
+      } else {
+        isPlaying ? pauseAudio(audio) : playAudio(audio);
+      }
+      return;
+    }
+
+    // if the audio is not in the sessionstorage, make a request to the openai api and play it
+    const mp3 = await openAIClient.audio.speech.create({
+      model: "tts-1",
+      input: script,
+      voice: "alloy",
+      response_format: "mp3",
+    });
+    const audioBlob = await mp3.arrayBuffer();
+    const audioURL = window.URL.createObjectURL(
+      new Blob([audioBlob], { type: "audio/mp3" })
+    );
+
+    createAndPlayAudio(audioURL);
+
+    // store the audio in the sessionstorage so that it can be replayed without making another request
+    sessionStorage.setItem("audio", audioURL);
+  };
   console.log(generatedScript);
+
   return (
     <section>
       <Title label="실행 결과" />
@@ -43,6 +91,10 @@ function GeneratedScript() {
           <Button
             label="Copy"
             onClick={() => handleClickCopy(generatedScript)}
+          />
+          <Button
+            label={isPlaying ? "멈춤" : "읽어주세요"}
+            onClick={() => handleClickTTS(generatedScript)}
           />
           <article className="border border-gray-300 rounded-md p-[16px] text-[14px] tracking-tight">
             <ul
