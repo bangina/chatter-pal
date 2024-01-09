9aa0f0a10849c38b666777be7b880846ebfabfab
bangina <bbangina@gmail.com>
2024-01-09 14:41:50 +0900

    WIP-Refactor GeneratedScript component and update system message in Prestudy page

--- src/components/prestudy/GeneratedScript.tsx
+++ src/components/prestudy/GeneratedScript.tsx
@@ -2,17 +2,41 @@ import { useRecoilValue } from "recoil";
 import Button from "../common/Button";
 import Title from "../common/Title";
 import { generatedScriptState } from "../../recoil";
-
 function GeneratedScript() {
   const generatedScript = useRecoilValue(generatedScriptState);
-
+  const parsedScript = (script: string) => {
+    if (!script.includes("[original_starts]")) return script;
+    return script
+      .replaceAll(
+        "[item_starts]",
+        "<li style='margin-bottom: 12px;'>"
+      )
+      .replaceAll("[item_ends]", "</li>")
+      .replaceAll(
+        "[original_starts]",
+        "<span style='color:red; text-decoration:line-through'>"
+      )
+      .replaceAll("[original_ends]", "</span>")
+      .replaceAll("[corrected_starts]", "<span style='color:green'>")
+      .replaceAll("[corrected_ends]", "</span>")
+      .replaceAll(
+        "[explanation_starts]",
+        "<p style='font-style:italic; margin-bottom: 12px;'> ➡ "
+      )
+      .replaceAll("[explanation_ends]", "</p>");
+  };
+  console.log(generatedScript);
   return (
     <section>
       <Title label="실행 결과" />
       <Button label="Copy" />
       <Button label="Highlight" />
-      <article className="border border-gray-300 rounded-md p-[16px] whitespace-pre-line">
-        <p>{generatedScript}</p>
+      <article className="border border-gray-300 rounded-md p-[16px] text-[14px] tracking-tight">
+        {generatedScript && (
+          <ul
+            dangerouslySetInnerHTML={{ __html: parsedScript(generatedScript) }}
+          />
+        )}
       </article>
     </section>
   );
--- src/pages/Prestudy.tsx
+++ src/pages/Prestudy.tsx
@@ -9,7 +9,21 @@ import { GeneratingType } from "../types/prestudy";
 const systemMessage = (type: GeneratingType) => {
   switch (type) {
     case GeneratingType.GRAMMAR:
-      return "Please correct grammar errors from my script provided above. I want the output to be like below; (1) full rewritten script(corrected texts should be font-weight 700 and red color text.) (2) bullet-point format list of grammar errors you corrected and detailed explanation on why each were corrected.";
+      return `"Please correct grammar errors and explain the reason in Korean sentence by sentence from my script provided above.
+      I want the output to be like below, so that I can parse the output easily. 
+      (1)each sentence starts with [item_starts] and ends with [item_ends].
+      (2)if there are no grammar errors in a sentence, please write "no error".
+      (2)original words that should be corrected starts with [original_starts] and ends with [original_ends].
+      (3)corrected words should be in [corrected_starts] and ends with [corrected_ends].
+      (4)corrected words should be attached to the original words in the original sentence.
+      (4)explanation on why each were corrected should be in [explanation_starts] and ends with [explanation_ends].
+      (5)no line break should be used.
+      (6)if there are multiple errors in a sentence, please correct them all.
+      
+      for example the output is like below;, 
+      [item_starts] I didn’t take it seriously because I assumed that [original_starts]it's[original_ends] [corrected_starts]it was[corrected_ends] just one of the common problems \n
+      [explanation_starts]the verb should be in past tense because the sentence is in past tense. Since the author is referring to a past event, "assumed" in the previous sentence is also in the past tense. [explanation_ends] [item_ends]
+      "`;
     case GeneratingType.VOCABULARY:
       return "Rewrite the answer provided above with more sophisticated, diverse, and professional vocabulary.";
     case GeneratingType.CONVERSATIONAL:
