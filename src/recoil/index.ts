d6d2b6e3b88f674d9d611a59fe7a1f11b4e8764a
bangina <bbangina@gmail.com>
2024-01-09 14:41:28 +0900

    Add sessionStorage for myScriptState and generatedScriptState

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,11 +1,35 @@
 import { atom } from "recoil";
 
-export const myScriptState = atom<string>({
-  key: "myScriptState",
-  default: "",
-});
+const atomWithSessionStorage = ({
+  key,
+  defaultVal,
+}: {
+  key: string;
+  defaultVal: string | null;
+}) => {
+  return atom<string | null>({
+    key,
+    default: defaultVal,
+    effects: [
+      ({ setSelf, onSet }) => {
+        const savedData = sessionStorage.getItem(key);
+        if (savedData) setSelf(JSON.parse(savedData));
 
-export const generatedScriptState = atom<string | null>({
+        onSet((newValue, _, isReset) => {
+          isReset
+            ? sessionStorage.removeItem(key)
+            : sessionStorage.setItem(key, JSON.stringify(newValue));
+        });
+      },
+    ],
+  });
+};
+
+export const myScriptState = atomWithSessionStorage({
+  key: "myScriptState",
+  defaultVal: "",
+});
+export const generatedScriptState = atomWithSessionStorage({
   key: "generatedScriptState",
-  default: "",
+  defaultVal: "",
 });
