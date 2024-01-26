70422fe30d80bb5ea181c8cf754c1420146bafbf
bangina <bbangina@gmail.com>
2024-01-26 17:01:33 +0900

    Add serverless configuration and debug script

---
+++ server/handler.ts
@@ -0,0 +1,35 @@
+import { APIGatewayEvent, Context } from "aws-lambda";
+import OpenAI from "openai";
+
+const env = <{ OPENAI_API_KEY: string }>process.env;
+type RequestBody = {
+  subject: string;
+};
+
+export async function main(event: APIGatewayEvent, context: Context) {
+  const body = <RequestBody>JSON.parse(event.body!);
+  if (!body || !body.subject)
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Invalid Request" }),
+    };
+  const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY });
+  const prompt = `Tell me a joke about ${body.subject}`;
+
+  const gptResponse = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+  const result = gptResponse.choices[0].message.content;
+
+  return {
+    statusCode: 200,
+    headers: {},
+    body: result,
+  };
+}
